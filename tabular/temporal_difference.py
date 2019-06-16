fee5b7c0b3793bef2f0903d9a255e2e9649a552b
Gregor Semmler <gregor.semmler@protonmail.com>
2019-06-16 14:33:37 +0200

    First untested implementation of StateActionTable

--- tabular/temporal_difference.py
+++ tabular/temporal_difference.py
@@ -1,5 +1,51 @@
 # -*- coding: utf-8 -*-
 import gym
+import heapq
+
+
+class StateActionValue(object):
+
+    def __init__(self, state, action):
+        self.state = state
+        self.action = action
+
+    def __hash__(self):
+        return hash((self.state, self.action))
+
+    def __eq__(self, other):
+        return (self.state, self.action) == (other.state, other.action)
+
+    def __ne__(self, other):
+        return not(self == other)
+
+
+class StateActionValueTable(object):
+
+    def __init__(self, default_value=0.0):
+        self.q = {}
+        self.default_value = default_value
+
+    def get_q_max(self, state):
+        q_values = self.get_q_values_from_state(state)
+        q_values = sorted(q_values.items(), key= lambda entry: -entry)  # Entry with highest value is first
+        if len(q_values) == 0:
+            return None
+        return q_values[0]
+
+    def get_q_values_from_state(self, state):
+        if state not in self.q:
+            self.q[state] = {}
+        return self.q[state]
+
+    def get_q_value(self, state, action):
+        if state not in self.q:
+            self.q[state] = {action: self.default_value}
+        return self.q[state][action]
+
+    def set_q_value(self, state, action, value):
+        if state not in self.q:
+            self.q[state] = {}
+        self.q[state][action] = value
 
 
 def simple_blackjack_policy(state):
@@ -34,13 +80,34 @@ def tabular_td0(env, policy, alpha=0.01, gamma=0.99, num_iterations=10000):
     return v
 
 
-def epsilon_greedy_tabular_sarsa(env, )
+def epsilon_greedy_tabular_sarsa(env, epsilon=0.1, alpha=0.5, gamma=0.99, num_iterations=10**5):
+    q = StateActionValueTable()
+
+    i = 0
+    while i < num_iterations:
+        state = env.reset()
+        action = env.action_space.sample()  # random first action
+        done = False
+
+        while not done:
+            pass
+            # TODO
+            # action = policy(state)
+            # new_state, reward, done, _ = env.step(action)
+            #
+            # if new_state not in v:
+            #     v[new_state] = 0.0  # initialize
+            #
+            # v[state] = v[state] + alpha * (reward + gamma * v[new_state] - v[state])
+        i += 1
+
+    pass
 
 
 def main():
     blackjack = gym.make("Blackjack-v0")
 
-    value_function = tabular_td0(blackjack, simple_blackjack_policy, alpha=1.0, gamma=1.0)
+    value_function = tabular_td0(blackjack, simple_blackjack_policy, alpha=0.5, gamma=1.0, num_iterations=10 ** 5)
 
     print("")
     pass
