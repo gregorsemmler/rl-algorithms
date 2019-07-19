3ca313fdd0d27fda8b47899828dd6fbfb3ee12fc
Gregor Semmler <gregor.semmler@protonmail.com>
2019-07-19 12:34:29 +0200

    Begin implementation of Monte Carlo Algorithms

---
+++ tabular/monte_carlo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import collections
+from enum import Enum
+
+import gym
+import numpy as np
+import re
+import logging
+from tensorboardX import SummaryWriter
+from gym import envs
+
+
+class MonteCarloAgent(object):
+
+    def __init__(self):
+        pass
+
+    def monte_carlo_prediction(self):
+        # TODO
+        pass
