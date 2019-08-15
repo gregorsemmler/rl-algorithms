5020aab8bafe8a54bfef53529212996a0bd5352c
Gregor Semmler <gregor.semmler@protonmail.com>
2019-08-15 12:12:41 +0200

    Begin refactoring of dynamic programming algorithms

--- tabular/dynamic_programming.py
+++ tabular/dynamic_programming.py
@@ -1,13 +1,16 @@
 import collections
-import logging
 from enum import Enum
 
-from tensorboardX import SummaryWriter
-
 import gym
+import numpy as np
+import re
+from math import inf
+import logging
+from tensorboardX import SummaryWriter
 from gym import envs
 
-from core import EpisodeResult
+from core import TabularPolicy, EpisodeResult, EpsilonGreedyTabularPolicy, StateActionValueTable, CustomPolicy
+from tabular.old_dynamic_programming import logger
 
 logger = logging.getLogger(__file__)
 
@@ -17,20 +20,15 @@ class DPAlgorithm(Enum):
     VALUE_ITERATION = "VALUE_ITERATION"
 
 
-class DPAgent(object):
+class EnvironmentEstimator(object):
 
     def __init__(self):
         self.rewards = collections.defaultdict(float)
         self.transitions = collections.defaultdict(collections.Counter)
         self.probabilities = collections.defaultdict(float)
-        self.policy = collections.defaultdict(int)
-        self.value_table = collections.defaultdict(float)
+        self.states = set()
 
-    # TODO refactor
-    def _select_action(self, state):
-        return self.policy[state]
-
-    def estimate_transition_probabilities(self, env, num_iterations=100):
+    def estimate(self, env, num_iterations=100):
         """
         Performs a number of random actions and estimates the state-action-state probabilities depending on how often
         they occurred during this time.
@@ -39,11 +37,13 @@ class DPAgent(object):
         :return:
         """
         state = env.reset()
+        self.states.update(state)
         for _ in range(num_iterations):
             action = env.action_space.sample()
             new_state, reward, is_done, _ = env.step(action)
             self.rewards[(str(state), action, str(new_state))] = reward
             self.transitions[(str(state), action)][str(new_state)] += 1
+            self.states.update(new_state)
             state = env.reset() if is_done else new_state
 
         for s, a in self.transitions.keys():
@@ -55,70 +55,82 @@ class DPAgent(object):
                 else:
                     self.probabilities[(s, a, s2)] = self.transitions[(s, a)][s2] / num_transitions
 
-    def policy_iteration(self, env, gamma=0.99, theta=0.001, num_random_steps=10 ** 3, max_iterations=10 ** 3):
+
+class DPAgent(object):
+
+    def __init__(self):
+        self.estimator = EnvironmentEstimator()
+        self.policy = None
+        self.v_table = None
+
+    # TODO test
+    def policy_iteration(self, env, policy=None, v=None, gamma=0.99, theta=0.001, num_exploration_steps=1000, max_iterations=1000):
         if not isinstance(env.action_space, gym.spaces.discrete.Discrete):
             raise RuntimeError("Expected Discrete action space")
 
-        self.estimate_transition_probabilities(env, num_iterations=num_random_steps)
+        if policy is None:
+            self.policy = TabularPolicy(random_defaults=env.action_space.n)
+        else:
+            self.policy = policy
 
-        v = self.value_table
-        policy = self.policy
+        if v is None:
+            self.v_table = collections.defaultdict(float)
+        else:
+            self.v_table = v
 
-        all_states = set([k[0] for k in self.probabilities.keys()]) | set([k[2] for k in self.probabilities.keys()])
+        self.estimator.estimate(env, num_iterations=num_exploration_steps)
+
+        all_states = self.estimator.states
 
         policy_stable = False
         i = 0
         while not policy_stable and i < max_iterations:
-            logger.warning("Round {}".format(i))
-            logger.debug("Policy Evaluation")
             # Policy evaluation
             delta = float("inf")
             while delta >= theta:
-                delta = 0
+                delta = 0.0
                 for s in all_states:
-                    val = v[s]
-                    v[s] = sum(
-                        [self.probabilities[(s, policy[s], s2)] * (self.rewards[(s, policy[s], s2)] + gamma * v[s2]) for
-                         s2
-                         in all_states])
-                    delta = max(delta, abs(val - v[s]))
-
-            logger.warning("Delta was {}".format(delta))
-            logger.debug("Policy Improvement")
+                    old_val = self.v_table[s]
+                    action = self.policy(s)
+                    new_val = sum([self.estimator.probabilities[(s, action, s2)] * (
+                            self.estimator.rewards[(s, action, s2)] + gamma * self.v_table[s2]) for s2 in
+                                   all_states])
+                    self.v_table[s] = new_val
+                    delta = max(delta, abs(old_val - new_val))
 
             # Policy improvement
             policy_stable = True
 
             for s in all_states:
-                old_action = policy[s]
+                old_action = self.policy(s)
 
-                state_action_pairs = [(st, ac) for (st, ac) in self.transitions.keys() if st == s]
                 best_action = None
                 best_value = float("-inf")
-                for _, a in state_action_pairs:
+                for _, a in [(st, ac) for (st, ac) in self.estimator.transitions.keys() if st == s]:
                     cur_val = 0.0
-                    for s2 in self.transitions[(s, a)].keys():
-                        cur_val += self.probabilities[(s, a, s2)] * (self.rewards[(s, a, s2)] + gamma * v[s2])
+                    for s2 in self.estimator.transitions[(s, a)].keys():
+                        cur_val += self.estimator.probabilities[(s, a, s2)] * (
+                                    self.estimator.rewards[(s, a, s2)] + gamma * self.v_table[s2])
 
                     if cur_val > best_value:
                         best_value = cur_val
                         best_action = a
                 if best_action is None:
                     logger.debug("No best action found for state {}".format(s))
-                    best_action = 0
-                policy[s] = best_action
-                if best_action != old_action:
+                    best_action = np.random.randint(env.action_space.n)
+                self.policy[s] = best_action
+                if best_action is not None and best_action != old_action:
                     logger.debug("policy[{}] -> {}".format(s, best_action))
                     policy_stable = False
             i += 1
 
-        self.policy = policy
-        self.value_table = v
+            if i % 100 == 0:
+                print("{} iterations done".format(i))
 
-    def learn(self, env, algorithm=DPAlgorithm.POLICY_ITERATION, gamma=0.99, theta=0.01, num_random_steps=10 ** 3,
-              max_iterations=10 ** 3):
+    def learn(self, env, algorithm=DPAlgorithm.POLICY_ITERATION, gamma=0.99, theta=0.01, num_random_steps=1000,
+              max_iterations=1000):
         if algorithm == DPAlgorithm.POLICY_ITERATION:
-            self.policy_iteration(env, gamma=gamma, theta=theta, num_random_steps=num_random_steps,
+            self.policy_iteration(env, gamma=gamma, theta=theta, num_exploration_steps=num_random_steps,
                                   max_iterations=max_iterations)
         elif algorithm == DPAlgorithm.VALUE_ITERATION:
             self.value_iteration(env, gamma=gamma, theta=theta, num_random_steps=num_random_steps)
@@ -140,7 +152,7 @@ class DPAgent(object):
                 if render:
                     env.render()
 
-                action = self._select_action(state)
+                action = self.policy(state)
                 new_state, reward, done, info = env.step(action)
                 new_state = str(new_state)
 
@@ -165,7 +177,7 @@ class DPAgent(object):
 
         self.estimate_transition_probabilities(env, num_iterations=num_random_steps)
 
-        v = self.value_table
+        v = self.v_table
         policy = collections.defaultdict(int)
 
         all_states = set([k[0] for k in self.probabilities.keys()]) | set([k[2] for k in self.probabilities.keys()])
@@ -185,7 +197,7 @@ class DPAgent(object):
                     if cur_val > best_value:
                         best_value = cur_val
 
-                if best_value == float("-inf") :
+                if best_value == float("-inf"):
                     logger.info("State {} has no best action".format(s))
                 v[s] = best_value
                 delta = max(delta, abs(val - v[s]))
@@ -209,52 +221,4 @@ class DPAgent(object):
             policy[s] = best_action
 
         self.policy = policy
-        self.value_table = v
-
-
-def main():
-    env_names = sorted(envs.registry.env_specs.keys())
-    env_name = "Taxi-v2"
-    algorithm = DPAlgorithm.POLICY_ITERATION
-    env_spec = envs.registry.env_specs[env_name]
-    environment = gym.make(env_name)
-    test_env = gym.make(env_name)
-
-    k = 0
-    goal_returns = env_spec.reward_threshold
-    gamma = 0.99
-
-    writer = SummaryWriter(comment="-{}-{}".format(env_name, algorithm))
-
-    max_rounds = 10000
-    agent = DPAgent()
-    test_best_result, test_best_return = None, float("-inf")
-    test_returns = []
-    num_random_steps = 1000
-    num_test_episodes = 100
-    while True:
-        agent.learn(environment, algorithm, gamma=gamma, num_random_steps=num_random_steps)
-        round_test_returns, round_test_best_result, round_test_best_return = agent.play(test_env, gamma=gamma,
-                                                                                        num_episodes=num_test_episodes)
-        for r_idx, r in enumerate(round_test_returns):
-            writer.add_scalar("test_return", r, len(test_returns) + r_idx)
-
-        test_returns.extend(round_test_returns)
-
-        if test_best_return < round_test_best_return:
-            test_best_return = round_test_best_return
-            test_best_result = round_test_best_result
-
-        average_test_return = 0.0 if len(round_test_returns) == 0 else sum(round_test_returns) / len(round_test_returns)
-        logger.warning("Average returns: {}".format(average_test_return))
-
-        if (goal_returns is not None and average_test_return >= goal_returns) or k >= max_rounds:
-            logger.warning("Done in {} rounds!".format(k))
-            break
-        k += 1
-
-    print("")
-
-
-if __name__ == "__main__":
-    main()
+        self.v_table = v
