ef4ae28f5e5c13fe8a7b6ab9b6b0a6b99f94e029
Gregor Semmler <gregor.semmler@protonmail.com>
2019-08-15 19:26:11 +0200

    Refactored Tabular Q Learning

--- tabular/temporal_difference.py
+++ tabular/temporal_difference.py
@@ -134,10 +134,46 @@ class TDAgent(object):
                 print("{} iterations done".format(i))
         pass
 
-    def learn(self, env, algorithm, num_iterations, policy=None, gamma=0.99, alpha=0.5, epsilon=0.1):
+    def q_learning(self, env, num_iterations=1000, gamma=0.99, alpha=0.5, b=None):
+        self.q_table = StateActionValueTable(possible_actions=range(env.action_space.n))
+        self.policy = TabularPolicy()
+
+        if b is not None:
+            behavior_policy = b
+        else:
+            behavior_policy = EpsilonGreedyTabularPolicy.random_policy(env.action_space.n)
+
+        i = 0
+        while i < num_iterations:
+            state = env.reset()
+            state = str(state)
+            done = False
+            episode_result = EpisodeResult(env, state)
+
+            while not done:
+                action = behavior_policy(state)
+                new_state, reward, done, _ = env.step(action)
+                new_state = str(new_state)
+                episode_result.append(action, reward, new_state)
+
+                update = reward + gamma * self.q_table.get_q_max(state) - self.q_table[state, action]
+                update *= alpha
+                self.q_table[state, action] += update
+                self.policy[state] = self.q_table.get_best_action(state)
+                state = new_state
+
+            i += 1
+
+            if i % 100 == 0:
+                print("{} iterations done".format(i))
+        pass
+
+    def learn(self, env, algorithm, num_iterations, policy=None, gamma=0.99, alpha=0.5, epsilon=0.1, b=None):
         if algorithm == TDAlgorithm.SARSA:
             self.tabular_sarsa(env, policy=policy, num_iterations=num_iterations, gamma=gamma, alpha=alpha,
                                epsilon=epsilon)
+        elif algorithm == TDAlgorithm.Q_LEARNING:
+            self.q_learning(env, num_iterations=num_iterations, gamma=gamma, alpha=alpha, b=b)
         else:
             raise ValueError("Unknown Prediction Algorithm: {}".format(algorithm))
 
@@ -159,9 +195,10 @@ def prediction():
 
 
 def control():
+    policy = TabularPolicy.sample_frozen_lake_policy()
     env_names = sorted(envs.registry.env_specs.keys())
-    env_name = "FrozenLake-v0"
-    algorithm = TDAlgorithm.SARSA
+    env_name = "Blackjack-v0"
+    algorithm = TDAlgorithm.Q_LEARNING
     env_spec = envs.registry.env_specs[env_name]
     environment = gym.make(env_name)
     test_env = gym.make(env_name)
@@ -178,7 +215,7 @@ def control():
     agent = TDAgent()
     test_best_result, test_best_return = None, float("-inf")
     test_returns = []
-    num_iterations = 1000
+    num_iterations = 10**4
     num_test_episodes = 100
     while True:
         agent.learn(environment, algorithm, num_iterations, gamma=gamma, alpha=alpha, epsilon=epsilon)
