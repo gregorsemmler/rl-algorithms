fe3b49b20f58f6e9f805c78d4dfb11e091253813
Gregor Semmler <gregor.semmler@protonmail.com>
2019-08-15 14:10:52 +0200

    Refactored Value Iteration

--- tabular/dynamic_programming.py
+++ tabular/dynamic_programming.py
@@ -66,7 +66,8 @@ class DPAgent(object):
         self.policy = None
         self.v_table = None
 
-    def policy_iteration(self, env, policy=None, v=None, b=None, gamma=0.99, theta=0.001, num_exploration_steps=1000, max_iterations=1000):
+    def policy_iteration(self, env, policy=None, v=None, b=None, gamma=0.99, theta=0.001, num_exploration_steps=1000,
+                         max_iterations=1000):
         if not isinstance(env.action_space, gym.spaces.discrete.Discrete):
             raise RuntimeError("Expected Discrete action space")
 
@@ -129,15 +130,56 @@ class DPAgent(object):
             if i % 100 == 0:
                 print("{} iterations done".format(i))
 
+    def value_iteration(self, env, policy=None, v=None, b=None, gamma=0.99, theta=0.001, num_exploration_steps=1000):
+        if not isinstance(env.action_space, gym.spaces.discrete.Discrete):
+            raise RuntimeError("Expected Discrete action space")
+
+        if policy is None:
+            self.policy = TabularPolicy(random_defaults=env.action_space.n)
+        else:
+            self.policy = policy
+
+        if v is None:
+            self.v_table = collections.defaultdict(float)
+        else:
+            self.v_table = v
+
+        self.estimator.estimate(env, b=b, num_iterations=num_exploration_steps)
+        all_states = sorted(self.estimator.states)
+
+        # Policy evaluation
+        delta = float("inf")
+        while delta >= theta:
+            delta = 0.0
+            for s in all_states:
+                old_val = self.v_table[s]
+                action = self.policy(s)
+                new_val = sum([self.estimator.probabilities[(s, action, s2)] * (
+                        self.estimator.rewards[(s, action, s2)] + gamma * self.v_table[s2]) for s2 in
+                               all_states])
+                self.v_table[s] = new_val
+                delta = max(delta, abs(old_val - new_val))
+
+        for s in all_states:
+            best_action = None
+            best_value = float("-inf")
+            for _, a in [(st, ac) for (st, ac) in self.estimator.transitions.keys() if st == s]:
+                cur_val = 0.0
+                for s2 in self.estimator.transitions[(s, a)].keys():
+                    cur_val += self.estimator.probabilities[(s, a, s2)] * (
+                            self.estimator.rewards[(s, a, s2)] + gamma * self.v_table[s2])
+
+                if cur_val > best_value:
+                    best_value = cur_val
+                    best_action = a
+            if best_action is None:
+                logger.debug("No best action found for state {}".format(s))
+                best_action = 0
+            self.policy[s] = best_action
+
     def learn(self, env, algorithm=DPAlgorithm.POLICY_ITERATION, gamma=0.99, theta=0.01, num_random_steps=1000,
               max_iterations=1000):
-        if algorithm == DPAlgorithm.POLICY_ITERATION:
-            self.policy_iteration(env, gamma=gamma, theta=theta, num_exploration_steps=num_random_steps,
-                                  max_iterations=max_iterations)
-        elif algorithm == DPAlgorithm.VALUE_ITERATION:
-            self.value_iteration(env, gamma=gamma, theta=theta, num_random_steps=num_random_steps)
-        else:
-            raise RuntimeError("Algorithm not implemented")
+        raise RuntimeError("Algorithm not implemented")
 
     def play(self, env, num_episodes=100, gamma=0.99, render=False):
         i = 0
@@ -173,61 +215,11 @@ class DPAgent(object):
 
         return episode_returns, best_result, best_return
 
-    def value_iteration(self, env, gamma=0.99, theta=0.01, num_random_steps=1000):
-        if not isinstance(env.action_space, gym.spaces.discrete.Discrete):
-            raise RuntimeError("Expected Discrete action space")
-
-        self.estimate_transition_probabilities(env, num_iterations=num_random_steps)
-
-        v = self.v_table
-        policy = collections.defaultdict(int)
-
-        all_states = set([k[0] for k in self.probabilities.keys()]) | set([k[2] for k in self.probabilities.keys()])
-        # Policy evaluation
-        delta = float("inf")
-        while delta >= theta:
-            delta = 0
-            for s in all_states:
-                state_action_pairs = [(st, ac) for (st, ac) in self.transitions.keys() if st == s]
-                best_value = float("-inf")
-                val = v[s]
-                for _, a in state_action_pairs:
-                    cur_val = 0.0
-                    for s2 in self.transitions[(s, a)].values():
-                        cur_val += self.probabilities[(s, a, s2)] * (self.rewards[(s, a, s2)] + gamma * v[s2])
-
-                    if cur_val > best_value:
-                        best_value = cur_val
-
-                if best_value == float("-inf"):
-                    logger.info("State {} has no best action".format(s))
-                v[s] = best_value
-                delta = max(delta, abs(val - v[s]))
-
-        for s in all_states:
-
-            state_action_pairs = [(st, ac) for (st, ac) in self.transitions.keys() if st == s]
-            best_action = None
-            best_value = float("-inf")
-            for _, a in state_action_pairs:
-                cur_val = 0.0
-                for s2 in self.transitions[(s, a)].keys():
-                    cur_val += self.probabilities[(s, a, s2)] * (self.rewards[(s, a, s2)] + gamma * v[s2])
-
-                if cur_val > best_value:
-                    best_value = cur_val
-                    best_action = a
-            if best_action is None:
-                logger.info("No best action found for state {}".format(s))
-                best_action = 0
-            policy[s] = best_action
-
-        self.policy = policy
-        self.v_table = v
-
-    def predict(self, env, policy, algorithm, num_iterations, gamma=0.99, v=None,  b=None):
+    def predict(self, env, policy, algorithm, num_iterations, gamma=0.99, v=None, b=None):
         if algorithm == DPAlgorithm.POLICY_ITERATION:
-            self.policy_iteration(env, policy, b=b, gamma=gamma, max_iterations=num_iterations)
+            self.policy_iteration(env, policy, b=b, v=v, gamma=gamma, max_iterations=num_iterations)
+        elif algorithm == DPAlgorithm.VALUE_ITERATION:
+            self.value_iteration(env, policy, b=b, v=v, gamma=gamma)
         else:
             raise ValueError("Unknown Prediction Algorithm: {}".format(algorithm))
 
@@ -235,7 +227,7 @@ class DPAgent(object):
 def prediction():
     policy = TabularPolicy.sample_frozen_lake_policy()
     env_name = "FrozenLake-v0"
-    algorithm = DPAlgorithm.POLICY_ITERATION
+    algorithm = DPAlgorithm.VALUE_ITERATION
     environment = gym.make(env_name)
 
     k = 0
